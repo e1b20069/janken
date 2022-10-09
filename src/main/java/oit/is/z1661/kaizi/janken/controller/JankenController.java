e785bfda5a2616736be2821e10ce00ff5032be01
e1b20069 <e1b20069@st.oit.ac.jp>
2022-10-10 05:17:13 +0900

    janken.htmlへの遷移の変更9

--- src/main/java/oit/is/z1661/kaizi/janken/controller/JankenController.java
+++ src/main/java/oit/is/z1661/kaizi/janken/controller/JankenController.java
@@ -2,11 +2,13 @@ package oit.is.z1661.kaizi.janken.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("profile")
 public class JankenController {
   @GetMapping("/janken")
-  public String sample21() {
+  public String janken() {
     return "janken.html";
   }
 }
