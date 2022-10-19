f160591471f26ec9ee103f2eb9d88e50fab61089
e1b20069 <e1b20069@st.oit.ac.jp>
2022-10-19 10:03:56 +0900

    消滅したlec03(第3回個人演習課題)のリペア

---
+++ src/main/java/oit/is/z1661/kaizi/janken/model/Entry.java
@@ -0,0 +1,28 @@
+package oit.is.z1661.kaizi.janken.model;
+
+import java.util.ArrayList;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Entry {
+  ArrayList<String> users = new ArrayList<>();
+  String loginname;
+  int roomNo = 1;
+
+  public void addUser(String name) {
+    for (String s : this.users) {
+      if (s.equals(name)) {
+        return;
+      }
+    }
+    this.users.add(name);
+  }
+
+  public ArrayList<String> getUsers() {
+    return users;
+  }
+
+  public void setUsers(ArrayList<String> users) {
+    this.users = users;
+  }
+}
